db312bf76edba09953bfb32d2e78223f4b47a0de
Alexander Vasilkov <a.vasilkov@voskhod.ru>
2020-01-24 19:42:01 +0300

    Minor changes

--- src/main/java/ru/voskhod/springsecutity/demo/config/DemoAppConfig.java
+++ src/main/java/ru/voskhod/springsecutity/demo/config/DemoAppConfig.java
@@ -18,7 +18,7 @@ public class DemoAppConfig {
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 
-        viewResolver.setPrefix("/WEB-INF/view");
+        viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
