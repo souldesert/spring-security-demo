1420ff2a8ff2c66648fc90c831da7e15fbd93f0b
Александр Васильков <y915@yandex.ru>
2020-01-25 15:51:12 +0300

    Spring security bootstrapped

---
+++ src/main/java/ru/voskhod/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package ru.voskhod.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// register spring security filters
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
